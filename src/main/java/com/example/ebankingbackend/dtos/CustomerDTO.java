bdaa83a3d32b4067bf8ead06b466b929c6460e6b
Yassine Archid <ARCHID_YASSINE@emsi-edu.ma>
2022-06-29 01:26:44 +0200

    proj1

---
+++ src/main/java/com/example/ebankingbackend/dtos/CustomerDTO.java
@@ -0,0 +1,21 @@
+package com.example.ebankingbackend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Data
+public class CustomerDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+}
+
